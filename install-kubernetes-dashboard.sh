9c43487ee958790b97182dc9c4fa0c6fc82826da
Evgeny Yakimov <john.yakimov@gmail.com>
2020-09-19 09:36:39 +0100

    Print token when installing dashboard

--- install-kubernetes-dashboard.sh
+++ install-kubernetes-dashboard.sh
@@ -1,3 +1,5 @@
 kubectl apply \
   -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml \
   -f kubernetes-dashboard-ingress.yaml
+TOKEN=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | awk '/^deployment-controller-token-/{print $1}') | awk '$1=="token:"{print $2}')
+echo "Authentication token is: $TOKEN"
